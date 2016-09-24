36ef7971722c1bda985b573bd2a0d211b20c9d58
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-23 19:16:51 -1000

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var email = require('./emailLog.json');
+
+/*console.log(email);*/
+
+function lengthEmail(){
+  var findEmail = email.emails;
+  var orgMail = [];
+  for (var i = 0; i < findEmail.length; i ++ ){
+    //console.log (findEmail[i]);
+    if(findEmail.indexOf(email, fromIndex = 0));
+      orgMail.push();
+    }
+}
+lengthEmail();
+
+module.exports = email;
